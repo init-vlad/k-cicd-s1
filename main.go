3e0235c110ebfaf587662d6e4eb283b655f0c2df
Vlad <vlad@init.red>
2025-06-23 21:12:51 +0500

    first commit

---
+++ main.go
@@ -0,0 +1,22 @@
+// ping.go
+package main
+
+import "net/http"
+
+func main() {
+	// clientID := os.Getenv("CLIENT_ID")
+	// if clientID == "" {
+	// 	clientID = "unknown"
+	// }
+	// for {
+	// 	log.Printf("PING 2.0 %s", clientID)
+	// 	time.Sleep(10 * time.Second)
+	// }
+
+	serv := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Service 1 alive"))
+	})
+
+	http.ListenAndServe(":8080", serv)
+}
